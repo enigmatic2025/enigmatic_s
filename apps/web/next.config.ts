2fdd0e94c6c52d172de0bfbc5111b293b4869463
Matcha Tea <enigmaticworks2025@gmail.com>
2026-09-11 16:01:30 +0700

    optimizing the pictures, video, cleanup

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 /** @type {import('next').NextConfig} */
 const nextConfig: NextConfig = {
   images: {
+    qualities: [60, 75, 85],
     remotePatterns: [
       {
         protocol: 'https',
